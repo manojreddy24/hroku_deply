9bc0f598f2d539ea561eb09491a18c1777c530b8
MANIDEEP2233 <manideepshanigaram.sm@gmail.com>
2022-11-26 18:08:20 -0600

    error

--- sec_prog/urls.py
+++ sec_prog/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sharefiles.urls')),
-    #url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    #url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
